90d27cdf8795e4d4f16f47390deef6d50f9258be
Lauren Birts <birtslauren@gmail.com>
2020-03-16 18:23:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,28 +3,23 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  result = num1 - num2
-  puts result
+  num1 - num2
 end
 
 def division(num1, num2)
-  result = num1 / num2
-  puts result
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  result = num1 * num2
-  puts result
+  num1 * num2
 end
 
 def modulo(num1, num2)
-  result = num1 % num2
-  puts result
+  num1 % num2
 end
 
 def square_root(num)
-  result = Math.sqrt(num)
-  puts result
+  Math.sqrt(num)
 end
 
 addition(5,4)
